b73df5f874c97c368c1496fd2a92d6da1325894c
Arut <arutyun-1996@yandex.ru>
2022-08-08 01:36:21 +0300

    init

--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 
 const passport = require('passport');
 
-const CLIENT_URL = 'https://elbrus-state.herokuapp.com';
+const CLIENT_URL = 'https://elbrus-state.herokuapp.com/main';
 
 router.get('/login/success', (req, res) => {
   if (req.user) {
